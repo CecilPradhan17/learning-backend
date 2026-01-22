8d4de2db7fd83c123e6f01311da3a24174a2f32e
CecilPradhan17 <cecilpradhan99@gmail.com>
2026-01-22 09:08:35 -0600

    {help and version lookup feature}

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,6 +2,16 @@ import { addTodo } from './commands/add.js';
 import { deleteTodo } from './commands/delete.js';
 import { listTodos } from './commands/list.js';
 import { updateTodo } from './commands/done.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pkg = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8')
+);
 
 const [ , , command, query ] = process.argv;
 
@@ -16,6 +26,17 @@ else if( command == '--list'){
 }
 else if( command == '--done'){
     updateTodo(query);
+}
+else if(command == '--help'){
+    console.log(`    "--new": to add a new todo item,
+    "--list [all|pending|done]": to list the todo items,
+    "--done [id]": to update a todo item,
+    "--delete [id]": to delete a todo item,
+    "--help": to list all the available options,
+    "--version": to print the version of the application`);
+}
+else if(command == '--version'){
+    console.log(pkg.version);
 }
 
 
